9956e41842b632910561e850b76094c5dafabb49
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-01 20:32:37 -0700

    product schema

--- models/product.js
+++ models/product.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxlength: 60,
+    },
+    desc: {
+        type: String,
+        required: true,
+        maxlength: 60,
+    },
+})
